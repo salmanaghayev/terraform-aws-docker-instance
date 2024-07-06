b5e3f9f321ac99c76d9bdff2913200430b257fc0
Salman Aghayev <salman.aghayev@gmail.com>
2024-07-06 12:39:58 -0400

    Cleaning main.tf security group ingress lines

--- main.tf
+++ main.tf
@@ -51,10 +51,7 @@ resource "aws_security_group" "tf-sec-gr" {
       to_port     = port.value
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
-      #   ipv6_cidr_blocks = []
-      #   prefix_list_ids  = []
-      #   security_groups  = []
-      self = false
+      self        = false
     }
   }
   egress = [{
